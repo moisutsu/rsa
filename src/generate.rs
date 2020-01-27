b1a799b78a12a08ba0419cbc91f6783cc6fa5664
j-hira <jun1513910@gmail.com>
2020-01-27 22:30:43 +0900

    素数判定関数を作成

--- src/generate.rs
+++ src/generate.rs
@@ -2,13 +2,36 @@ extern crate rand;
 use rand::Rng;
 
 pub fn generate() {
-    println!("{:0>1$b}", generate_integer(), 128);
     println!("generate");
+    let mut x;
+    loop {
+        x = generate_integer();
+        if is_prime(x) {
+            break;
+        }
+    }
+    println!("{}", x);
 }
 
-fn generate_integer() -> u128 {
-    let mut x: i128 = rand::thread_rng().gen();
+fn generate_integer() -> u32 {
+    let mut x: i32 = rand::thread_rng().gen();
     x = if x < 0 {x} else {-x};
-    let y: u128 = x as u128;
+    let y: u32 = x as u32;
     if y % 2 == 0 {y + 1} else {y}
+}
+
+fn is_prime(num: u32) -> bool {
+    if num == 2 {
+        return true
+    }
+    if num < 2 || num & 1 == 0 {
+        return false
+    }
+    let upper_limit = (num as f64).sqrt() as u32 + 1;
+    for i in (3..upper_limit).step_by(2) {
+        if num % i == 0 {
+            return false
+        }
+    }
+    true
 }

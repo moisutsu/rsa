8a73492457b78897b590c703c36010776f6c28e9
j-hira <jun1513910@gmail.com>
2020-01-27 23:15:07 +0900

    鍵のペアを生成する関数を作成

--- src/generate.rs
+++ src/generate.rs
@@ -2,11 +2,39 @@ extern crate rand;
 use rand::Rng;
 
 pub fn generate() {
-    println!("generate");
-    println!("{}", generate_prime());
+    let p = generate_prime();
+    let q = generate_prime();
+    let n = p * q;
+    let l = lcm(p - 1, q - 1);
+    let e = generate_e(l);
+    let d = generate_d(l, e);
+
+    println!("Public key");
+    println!("({}, {})", e, n);
+    println!("Secret key");
+    println!("({}, {})", d, n);
 }
 
-fn generate_prime() -> u32 {
+fn generate_d(l: u128, e: u128) -> u128 {
+    let mut i = 1;
+    loop {
+        if (i * l + 1) % e == 0 {
+            return (i * l + 1) / 3;
+        }
+        i += 1;
+    }
+}
+
+fn generate_e(l: u128) -> u128 {
+    for i in 2..l-1 {
+        if gcd(i, l) == 1 {
+            return i;
+        }
+    }
+    0
+}
+
+fn generate_prime() -> u128 {
     let mut x;
     loop {
         x = generate_integer();
@@ -14,7 +42,7 @@ fn generate_prime() -> u32 {
             break;
         }
     }
-    x
+    x as u128
 }
 
 fn generate_integer() -> u32 {
@@ -38,4 +66,19 @@ fn is_prime(num: u32) -> bool {
         }
     }
     true
+}
+
+fn gcd(p: u128, q: u128) -> u128 {
+    let (mut a, mut b) = if p > q {(p, q)} else {(q, p)};
+    let mut r = a % b;
+    while r != 0 {
+        a = b;
+        b = r;
+        r = a % b;
+    }
+    b
+}
+
+fn lcm(p: u128, q: u128) -> u128 {
+    p * q / gcd(p, q)
 }

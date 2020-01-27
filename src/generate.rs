c017fd87cb45c85acf52ea52c9e4d57522284ad7
j-hira <jun1513910@gmail.com>
2020-01-27 21:35:31 +0900

    MSB,LSBが1の整数を生成する関数を作成

--- src/generate.rs
+++ src/generate.rs
@@ -1,3 +1,14 @@
+extern crate rand;
+use rand::Rng;
+
 pub fn generate() {
+    println!("{:0>1$b}", generate_integer(), 128);
     println!("generate");
+}
+
+fn generate_integer() -> u128 {
+    let mut x: i128 = rand::thread_rng().gen();
+    x = if x < 0 {x} else {-x};
+    let y: u128 = x as u128;
+    if y % 2 == 0 {y + 1} else {y}
 }
